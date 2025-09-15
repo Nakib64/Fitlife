c79cb6362a7bff0251bce9fd56a0debce71845e2
Saklain Mostak <saklainmostak926@gmail.com>
2025-09-15 23:33:53 +0600

    Added a demo description section

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Navbar from "./component/Navbar";
 import Footer from "./component/Footer";
+import Description from "./component/Description";
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <nav>
         <Navbar></Navbar>
+        <Description></Description>
       </nav>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
