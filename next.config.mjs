dada83d190959fac1b42f6b761a3f7013c5ac87d
Ayon203008 <paypal572874@gmail.com>
2025-09-29 21:39:18 +0600

    review section update

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 import createNextIntlPlugin from 'next-intl/plugin';
-const nextConfig = {
-    images: {
-    domains: ["i.ibb.co.com","i.ibb.co",], 
-  },
-};
+const nextConfig = {  images: {
+    remotePatterns: [{
+        protocol:'https',
+        hostname:"*"
+    }],
+  },};
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
 
