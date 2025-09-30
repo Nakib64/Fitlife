cb9c54294b689753675affbf13e740c9d6149fe2
Nakib64 <nafiz2282@gmail.com>
2025-09-30 23:22:39 +0600

    translated hero and description section

--- src/app/[locale]/component/Navbar/Navbar.jsx
+++ src/app/[locale]/component/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { Menu, X } from "lucide-react";
 import Image from "next/image";
-import Link from "next/link";
+import { Link } from "@/i18n/navigation";
 import React, { useState, useEffect } from "react";
 import { motion, useAnimation } from "framer-motion";
 import UserInfo from "../userInfo/UserInfo";
